17da799096f4b49d983e5a7895a9f4d9fbde584f
askarf <ayalakarf@gmail.com>
2023-10-20 12:49:47 -0400

    added three controllerone controller and three actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  def hello_method
+    render json: { message: "Hey there!" }
+  end
+
+  def goodbye_method
+    render json: { message: "See ya later alligator!!" }
+  end
+
+  def not_available_method
+    render json: { message: "Sorry, i'm not available now:(" }
+  end
 end
